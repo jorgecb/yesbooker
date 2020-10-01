3033780d0660aa0a7351bd298e432682f6c92009
chuy <jesus.za@yespointos.com>
2020-10-01 14:43:02 -0500

    validacion y campos limpios

---
+++ src/database/Sucursales.ts
@@ -0,0 +1,32 @@
+import db from './database';
+
+const tableName = 'Sucursales';
+
+const Sucursal ={
+    add(data:any){
+        db.table(tableName)
+            .add(data)
+            .then((id)=>{
+                return data;
+            });
+    },
+    listAll(){
+        var data = db.table(tableName)
+            .toArray()
+            .then((todos)=>{
+                return todos;
+            })
+            .catch((err) =>{
+                return err;
+            });
+            return data;
+    },
+    update(id:any,data:any){
+        db.table(tableName)
+            .update(id, data)
+            .then(() =>{
+                return data;
+            });
+    }
+}
+export default Sucursal;
--- src/database/database.ts
+++ src/database/database.ts
@@ -5,6 +5,15 @@ db.version(1).stores(
     Usuarios: '++id, nombre, materno, email, inserver',
     Socios: '++id, nombre_socio, email, inserver',
     Zonas: '++id, nombre_zona, descripcion, inserver',
+    Sucursales: '++id, nombre_sucursal, direccion, inserver',
+    Reservas:'++id, idcliente, idzona, idsucursal, inserver',
     });
-
+    db.version(10).stores(
+        { 
+        Usuarios: '++id, nombre, materno, email, inserver',
+        Socios: '++id, nombre_socio, email, inserver',
+        Zonas: '++id, nombre_zona, descripcion, inserver',
+        Sucursales: '++id, nombre_sucursal, direccion, inserver',
+        Reservas:'++id, idcliente, idzona, idsucursal, inserver',
+        });
 export default db; 
