09d31e4d5bf0a05c87a1a43db84cae327aaa2332
Eugenio <egjand1@gmail.com>
2020-10-01 18:46:57 -0500

    fusion con socios

--- src/components/button/CodigoPais.tsx
+++ src/components/button/CodigoPais.tsx
@@ -1,63 +1,82 @@
 import React from 'react';
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import MenuItem from '@material-ui/core/MenuItem';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import TextField from "@material-ui/core/TextField";
+import Menu from '@material-ui/core/Menu';
 
-const options = [
-  'Español',
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.palette.background.paper,
+    },
+  }),
+);
+
+const idiomas = [
+  "selecciona tu idioma",
   'Ingles',
   'Frances',
-  'Italiano',
-  'Aleman',
-  'Chino',
   'Portugues',
+  'Español',
 ];
 
-const ITEM_HEIGHT = 48;
+export default function SimpleListMenu() {
+  
 
-export default function LongMenu() {
+  const classes = useStyles();
+  
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const [selected, setSelected] = React.useState("Selecciona tu idioma");
+
+  const handleClickListItem = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
-
+  const handleMenuItemClick = (event: React.MouseEvent<HTMLElement>, index:any) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
+    setSelected(idiomas[index]);
+  };
   const handleClose = () => {
     setAnchorEl(null);
   };
+  console.log(selected)
+
+ /*  props.create({
+    Idioma: selectedIndex.Idioma
+  }); */
+
 
   return (
-    <div>
-      <IconButton
-        aria-label="more"
-        aria-controls="long-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        <TextField
-          label="Idioma"
-          value="seleccione su idioma"
-          
-        />
-      </IconButton>
+    <div className={classes.root}>
+      <List component="nav" aria-label="Device settings">
+        <ListItem
+          button
+          aria-haspopup="true"
+          aria-controls="lock-menu"
+          onClick={handleClickListItem}
+        >
+          <ListItemText primary={selected} secondary={selected} />
+        </ListItem>
+      </List>
       <Menu
-        id="long-menu"
+        id="lock-menu"
         anchorEl={anchorEl}
         keepMounted
-        open={open}
+        open={Boolean(anchorEl)}
         onClose={handleClose}
-        PaperProps={{
-          style: {
-            maxHeight: ITEM_HEIGHT * 4.5,
-            width: '20ch',
-          },
-        }}
       >
-        {options.map((option) => (
-          <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+        {idiomas.map((option, index) => (
+          <MenuItem
+            key={option}
+            disabled={index === 0}
+            selected={index === selectedIndex}
+            onClick={(event) => handleMenuItemClick(event, index)}
+          >
             {option}
           </MenuItem>
         ))}
