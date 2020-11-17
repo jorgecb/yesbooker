2cd576b307104f9a839ed49b3f5ee65f36f0612c
Jesus CZ <jesus.za@yespointos.com>
2020-11-17 15:11:30 -0600

    thunk middleware habilitado

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,6 +1,7 @@
-import { createStore, combineReducers, applyMiddleware  } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose  } from 'redux';
 import thunk from 'redux-thunk';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { sucursalesReducer } from '../reducers/sucursalesReducer';
 import { sociosReducer } from '../reducers/sociosReducer';
 import { usuariosReducer } from '../reducers/usuariosReducer';
@@ -9,8 +10,8 @@ const reducers = combineReducers({
     socios: sociosReducer,
     usuarios: usuariosReducer,
 });
+const middlewareEnhancers = applyMiddleware(thunk);
+const composedEnharcers = composeWithDevTools(...[middlewareEnhancers]);
 export const store = createStore(
-    reducers,
-    devToolsEnhancer(
-        name
-   ));
+    reducers, composedEnharcers
+   );
