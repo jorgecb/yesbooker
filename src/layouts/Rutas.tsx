f1c67a5bd5fec16a126a78dd6cb514629dba1942
editkaos <editkaos.design@gmail.com>
2020-11-19 15:20:32 -0600

    loginporcorregir

--- src/layouts/Rutas.tsx
+++ src/layouts/Rutas.tsx
@@ -4,7 +4,7 @@ import { Route, Redirect } from 'react-router-dom';
 function PrivateRoute({ component: Component, roles, ...rest }: any) {
     return (
         <Route {...rest} render={props => {
-            if (!localStorage.getItem('usuarios')) {
+            if (!localStorage.getItem('UserCredenciales')) {
                 return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
             }
             
@@ -16,7 +16,7 @@ function PrivateRoute({ component: Component, roles, ...rest }: any) {
 function LoginRoute({ component: Component, roles, ...rest }: any) {
     return (
         <Route {...rest} render={props => {
-            if (localStorage.getItem('usuarios')) {
+            if (localStorage.getItem('UserCredenciales')) {
                 return <Redirect to={{ pathname: '/admin', state: { from: props.location } }} />
             }
             
